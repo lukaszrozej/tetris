4f3664d8e58da772f9f817f397c6ab4dcce734bd
Łukasz Rożej <lukasz.rozej@gmail.com>
2019-08-22 13:57:23 +0200

    Add tetronimos

--- game-logic.js
+++ game-logic.js
@@ -2,5 +2,91 @@ const width = 10
 const height = 20
 
 const root = document.querySelector(':root')
-root.setProperty('--well-rows', height)
-root.setProperty('--well-columns', width)
+root.style.setProperty('--well-rows', height)
+root.style.setProperty('--well-columns', width)
+
+let id = 0
+
+const tetronimos = [
+  {
+    type: 'I',
+    axis: { x: 5, y: 1 },
+    squares: [
+      { x: 3, y: 0 },
+      { x: 4, y: 0 },
+      { x: 5, y: 0 },
+      { x: 6, y: 0 }
+    ]
+  },
+  {
+    type: 'o',
+    axis: { x: 5, y: 1 },
+    squares: [
+      { x: 4, y: 0 },
+      { x: 5, y: 0 },
+      { x: 4, y: 1 },
+      { x: 5, y: 1 }
+    ]
+  },
+  {
+    type: 'T',
+    axis: { x: 4.5, y: 0.5 },
+    squares: [
+      { x: 3, y: 0 },
+      { x: 4, y: 0 },
+      { x: 5, y: 0 },
+      { x: 4, y: 1 }
+    ]
+  },
+  {
+    type: 'L',
+    axis: { x: 4.5, y: 0.5 },
+    squares: [
+      { x: 3, y: 0 },
+      { x: 4, y: 0 },
+      { x: 5, y: 0 },
+      { x: 3, y: 1 }
+    ]
+  },
+  {
+    type: 'J',
+    axis: { x: 4.5, y: 0.5 },
+    squares: [
+      { x: 3, y: 0 },
+      { x: 4, y: 0 },
+      { x: 5, y: 0 },
+      { x: 5, y: 1 }
+    ]
+  },
+  {
+    type: 'S',
+    axis: { x: 4.5, y: 0.5 },
+    squares: [
+      { x: 4, y: 0 },
+      { x: 5, y: 0 },
+      { x: 3, y: 1 },
+      { x: 4, y: 1 }
+    ]
+  },
+  {
+    type: 'Z',
+    axis: { x: 4.5, y: 0.5 },
+    squares: [
+      { x: 3, y: 0 },
+      { x: 4, y: 0 },
+      { x: 4, y: 1 },
+      { x: 5, y: 1 }
+    ]
+  }
+]
+
+const pickRandom = array => array[Math.floor(Math.random() * array.length)]
+
+const rndTetronimo = () => {
+  const tetronimo  = pickRandom(tetronimos)
+  const squares = tetronimo.squares.map(p => ({ ...p, id: id++ }))
+  return { ...tetronimo, ...{ squares } }
+}
+
+const initialState = {
+}
